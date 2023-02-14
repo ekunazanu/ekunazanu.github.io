f0545538047c3085fb1fa2851af81a3681252354
artfrowl <26524638+artfrowl@users.noreply.github.com>
2023-02-14 09:31:35 +0530

    stuff

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,10 @@ if [ "$#" -ne 1 ]
 fi
 
 date="$(date '+%Y-%m-%d')"
-title="$(sed '-s' $1.md)"
+title="$(head -1 ./resources/blogs/md/"$1.md" | cut -c 3-)"
 
-# content="$(md2html "$1.md")"
-# sed "s/$content/{{ content }}/" "$main.template.html" > "$1.html"
+markdown -f "links,image,html,fencedcode,noext,nocdata,superscript,del,autolink,tabstop" "./resources/blogs/md/$1.md" | tail +3 > "./blogs/$1.html.tmp"
+sed -e "25r ./blogs/$1.html.tmp" -e "s/{date}/$date/" -e "s/{title}/$title/" "./resources/site/main.template.html" > "./blogs/$1.html"
+rm "./blogs/$1.html.tmp"
+
+sed -i "25r" "./extras/index.html"
